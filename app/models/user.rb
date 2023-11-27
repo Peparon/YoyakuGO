bc838459e54ed829f16df6e69d9a51e860da3f24
Peparon <peparoncino@gmail.com>
2023-11-27 02:35:57 +0000

    ユーザーのフルネーム定義追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  
+  
+  def full_name
+    "#{last_name} #{first_name}"
+  end
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
